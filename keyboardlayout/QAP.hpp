16c58a1d779ca3844bea42a825c0ab70f2d40df1
Fred Sundvik <fsundvik@gmail.com>
2015-09-03 09:39:16 +0300

    Evaluator and file reading for QAP

--- keyboardlayout/QAP.hpp
+++ keyboardlayout/QAP.hpp
@@ -8,11 +8,46 @@ class QAP : public Objective<NumLocations>
 public:
 	QAP(const std::string& filename)
 	{
-
+		std::ifstream stream(filename);
+		std::getline(stream, std::string());
+		std::getline(stream, std::string());
+		for (size_t i = 0; i < NumLocations; i++)
+		{
+			for (size_t j = 0; j < NumLocations; j++)
+			{
+				stream >> m_distances[i][j];
+			}
+			std::getline(stream, std::string());
+		}
+		std::getline(stream, std::string());
+		for (size_t i = 0; i < NumLocations; i++)
+		{
+			for (size_t j = 0; j < NumLocations; j++)
+			{
+				stream >> m_flow[i][j];
+			}
+			std::getline(stream, std::string());
+		}
 	}
 
 	float evaluate(const Keyboard<NumLocations>& keyboard) const override
 	{
-		return 0.0f;
+		uint64_t sum = 0;
+		for (size_t i = 0; i < NumLocations; i++)
+		{
+			for (size_t j = 0; j < NumLocations; j++)
+			{
+				int a = keyboard.m_keys[i];
+				int b = keyboard.m_keys[j];
+				int dist = m_distances[i][j];
+				int flow = m_flow[a][b];
+				sum += dist * flow;
+			}
+		}
+		return -static_cast<float>(sum);
 	}
+private:
+	std::array<std::array<int, NumLocations>, NumLocations> m_distances;
+	std::array<std::array<int, NumLocations>, NumLocations> m_flow;
+
 };
