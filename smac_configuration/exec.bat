17eaf4fb50adc9ea7ece98b388799f0c130e4c15
Fred Sundvik <fsundvik@gmail.com>
2015-09-12 21:39:58 +0300

    Validation of QAP instances found by smac
    The validation is done with different number of evaluations.

--- smac_configuration/exec.bat
+++ smac_configuration/exec.bat
@@ -1,6 +1,11 @@
 @echo off
-set BEGINNING=--smac --test %1 --instance_info %2 --cutoff_time %3 --cutoff_length %4 --seed %5
-for /f "tokens=5,* delims= " %%a in ("%*") do set REST=%%b
+set temp=%1
+set instance=%temp:"=%
+set BEGINNING=--smac --test %instance% --instance_info %2 --cutoff_time %3 --cutoff_length %4 --seed %5
+for /f delims^=^"^ tokens^=2 %%a in ("%*") do set REST1=%%a
+for /f "tokens=4,* delims= " %%a in ("%REST1%") do set REST=%%b
 rem echo "Command line parameters"
 rem echo %BEGINNING% %REST%
+rem echo "Input"
+rem echo %*
 ..\x64\release\keyboardlayout.exe %BEGINNING% %REST%
