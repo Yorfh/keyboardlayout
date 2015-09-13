f1b3f28516888d7791412d541820ecb6239f4122
Fred Sundvik <fsundvik@gmail.com>
2015-09-13 15:07:41 +0300

    Don't let stagnation decrease the perturbation
    Also, bigger range for perturbation strength

--- keyboardlayout/BMAOptimizer.hpp
+++ keyboardlayout/BMAOptimizer.hpp
@@ -289,7 +289,8 @@ protected:
 				if (iterWithoutImprovement > m_stagnationAfter)
 				{
 					iterWithoutImprovement = 0;
-					perturbStr = std::max<size_t>(static_cast<size_t>(KeyboardSize * stagnationDistribution(m_randomGenerator)), 2);
+					auto str = std::max<size_t>(static_cast<size_t>(KeyboardSize * stagnationDistribution(m_randomGenerator)), 2);
+					perturbStr = std::max(str, perturbStr);
 				}
 				else if (hasImproved == true && prevLocalOptimum != currentKeyboard) // Escaped from the previous local optimum. New local optimum reached
 				{
