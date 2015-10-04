4a9461e910462dbb5f3cee88655a27d412859b97
Fred Sundvik <fsundvik@gmail.com>
2015-10-04 18:32:59 +0300

    Copying of the last result
    The last result of the smac optimization is copied to the
    optimized_parameters directory.

--- smac_configuration/analyze_validation_results.py
+++ smac_configuration/analyze_validation_results.py
@@ -9,10 +9,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int)
 parser.add_argument("--scenario-file", type=str)
 parser.add_argument("--output-dir", type=str)
+parser.add_argument("--copy-last", action="store_true")
 args = parser.parse_known_args(sys.argv)[0]
 
 scenario = args.scenario_file
-scenario = os.path.split(scenario)[1]
+scenario_dir, scenario = os.path.split(scenario)
 scenario = os.path.splitext(scenario)[0]
 outputdir = args.output_dir
 
@@ -32,6 +33,16 @@ else:
 
 filename = os.path.abspath(filename)
 filename = os.path.normpath(filename)
+
+if args.copy_last:
+    last_dir = os.path.join(scenario_dir, "optimized_parameters")
+    last_file = os.path.join(last_dir, "last.txt")
+    traj_run = os.path.join(outputdir, scenario, "traj-run-%s.txt" % args.seed)
+    with open(traj_run) as f:
+        lines = f.readlines()
+        with open(last_file, "w") as f2:
+            f2.writelines(lines[0] + lines[-1])
+
 with open(filename) as f:
     instances = defaultdict(lambda: defaultdict(list))
     reader = csv.DictReader(f)
--- smac_configuration/smac.bat
+++ smac_configuration/smac.bat
@@ -1,3 +1,3 @@
 set seed=%RANDOM%
 call ../smac/smac.bat --seed %seed% --output-dir output --scenario-file %*
-c:\python27\python analyze_validation_results.py --seed %seed% --output-dir output --scenario-file %*
+c:\python27\python analyze_validation_results.py --seed %seed% --output-dir output --copy-last --scenario-file %*
