2efe3ddaf9b3af66e8974c30ae2518ce56dd1dcb
Fred Sundvik <fsundvik@gmail.com>
2016-01-10 21:52:17 +0200

    The pysmac runner transforms the result to SAT
    After timeout the result is assumed to be satisfied, and transformed
    to SAT. The time of the best found is used.

--- smac_configuration/pysmac/pysmac_tests.py
+++ smac_configuration/pysmac/pysmac_tests.py
@@ -58,6 +58,14 @@ class RunnerTests(unittest.TestCase):
         runner = Runner("keyboardlayout.exe", ["run_instance.py", "test_instance --param1 4", "instance_info", "cutoff_time", "cutoff_length", "seed", "--first_extra_arg", "--second_extra_arg"])
         self.assertEqual(runner.cmd, "keyboardlayout.exe --smac --test test_instance --param1 4 --instance_info instance_info --cutoff_time cutoff_time --cutoff_length cutoff_length --seed seed --first_extra_arg --second_extra_arg")
 
+    def test_a_timeout_is_transformed_to_sat(self):
+        result = "Result of this algorithm run: TIMEOUT, 1, 0.359375, 1818146, 9310539, 0"
+        self.assertEqual(Runner.transform_timeout_to_sat(result), "Result of this algorithm run: SAT, 0.359375, 0.359375, 1818146, 9310539, 0")
+
+    def test_a_sat_is_not_transformed(self):
+        result = "Result of this algorithm run: SAT, 0.359375, 0.359375, 1818146, 9310539, 0"
+        self.assertEqual(Runner.transform_timeout_to_sat(result), "Result of this algorithm run: SAT, 0.359375, 0.359375, 1818146, 9310539, 0")
+
 
 if __name__ == '__main__':
     unittest.main()
--- smac_configuration/pysmac/runner.py
+++ smac_configuration/pysmac/runner.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 class Runner:
     def __init__(self, exe, argv):
@@ -9,9 +10,18 @@ class Runner:
     def run(self):
         p = subprocess.Popen(self.cmd, stdout=subprocess.PIPE)
         stdoutdata, _ = p.communicate()
-        # TODO, if we didn't specify a target, then assume success, and change the result
-        # TODO Make keyboardlayout.exe output the time when the best solution was found so it can be used
         # TODO(optional), if the result was better than target, then abort everything, remove all results for that instance
         # and restart
-        return stdoutdata
+        return Runner.transform_timeout_to_sat(stdoutdata)
+
+    @classmethod
+    def transform_timeout_to_sat(cls, res):
+        if res.find("TIMEOUT"):
+            res = res.replace("Result of this algorithm run: ", "")
+            output = res.split(", ")
+            output[0] = "SAT"
+            output[1] = output[2]
+            res = "Result of this algorithm run: " + ", ".join(output)
+
+        return res
 
